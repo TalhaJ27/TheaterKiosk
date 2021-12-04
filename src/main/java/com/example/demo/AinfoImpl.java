02796f5311c89ed4a2a84c579f226a72b33f7099
Talha <36200558+TalhaJ27@users.noreply.github.com>
2021-12-04 15:32:33 -0500

    Trying to implement Proxy Pattern -Talha

---
+++ src/main/java/com/example/demo/AinfoImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class AinfoImpl implements Ainfo {
+
+
+    @Override
+    public void getAccountInfo() {
+        System.out.println("Performing work in real object");
+    }
+}
