7beaf53317018c9cab321e4bfeccf61fc08f208a
rahman-ashik <ashik.shovon03@gmail.com>
2021-12-04 20:01:19 -0500

    JUnit init from Rahman

---
+++ src/main/java/com/example/demo/GUI_ControllerTest.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+import org.junit.Test;
+
+class GUI_ControllerTest {
+
+    @Test
+    void onViewAllBooking() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void onNewBookingButtonClick() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void onCancelBookingButtonClick() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void onAccountInfoButtonClick() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void onDeleteAccountButtonClick() {
+    }
+}
